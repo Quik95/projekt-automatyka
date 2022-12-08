526f5ee9393faa201fcabece4868d8feb7b1805f
Hoaqin <michaldomasik@wp.pl>
2022-12-08 17:22:29 +0100

    Added graph

--- frontend/src/components/graph/graph.js
+++ frontend/src/components/graph/graph.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Plot from "react-plotly.js";
 
-export default function graph() {
-  //controller
-  const headers = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,POST,PATCH,OPTIONS",
-  };
-  fetch(
-    "http://localhost:8000/controller?" +
-      new URLSearchParams({
-        simulationTime: 10,
-        startingTemperature: 0,
-        desiredTemperature: 25,
-        Kp: 2.0,
-        Ki: 0.6,
-        Kd: 0.05,
-        maxHvacOutput: 10000,
-      }),
-    { headers: headers, method: "OPTIONS" }
-  )
-    .then((response) => response.json())
-    .then((data) => {
+export default function Graph() {
+  const [data, setData] = useState({ graphData: [] });
+
+  useEffect(() => {
+    async function getData() {
+      const headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PATCH,OPTIONS",
+      };
+      const data = await fetch(
+        "http://localhost:8000/controller?" +
+          new URLSearchParams({
+            simulationTime: 10,
+            startingTemperature: 0,
+            desiredTemperature: 25,
+            Kp: 2.0,
+            Ki: 0.6,
+            Kd: 0.05,
+            maxHvacOutput: 10000,
+          }),
+        { headers: headers, method: "OPTIONS" }
+      ).then((response) => {
+        return response.json();
+      });
+      setData(data);
       console.log(data);
-    });
+    }
+    getData();
+  }, []);
 
-  return <h1>dupa</h1>;
+  return (
+    <Plot
+      data={[
+        {
+          x: [...data.times],
+          y: [...data.temps],
+          type: "scatter",
+          mode: "lines",
+        },
+      ]}
+    ></Plot>
+  );
 }
