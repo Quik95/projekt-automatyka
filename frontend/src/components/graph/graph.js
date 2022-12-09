ed05deb884defc19a9ef1b784898161b90e85403
Hoaqin <michaldomasik@wp.pl>
2022-12-09 11:10:12 +0100

    Added sliders with css

--- frontend/src/components/graph/graph.js
+++ frontend/src/components/graph/graph.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Plot from "react-plotly.js";
-
+import Fields from "../fields/Fields.js";
+import styles from "./graph.css";
 export default function Graph() {
   const [data, setData] = useState({ graphData: [] });
+  const [startingTemperature, setStartingTemperature] = useState(0);
+  const [desiredTemperature, setDesiredTemperature] = useState(0);
+  const [Kp, setKp] = useState(1.0);
+  const [Ki, setKi] = useState(1.5);
+  const [Kd, setKd] = useState(2.0);
 
   useEffect(() => {
     async function getData() {
@@ -14,13 +20,13 @@ export default function Graph() {
       const data = await fetch(
         "http://localhost:8000/controller?" +
           new URLSearchParams({
-            simulationTime: 10,
-            startingTemperature: 0,
-            desiredTemperature: 25,
-            Kp: 2.0,
-            Ki: 0.6,
-            Kd: 0.05,
-            maxHvacOutput: 10000,
+            simulationTime: 5000,
+            startingTemperature: startingTemperature,
+            desiredTemperature: desiredTemperature,
+            Kp: parseFloat(Kp),
+            Ki: parseFloat(Ki),
+            Kd: parseFloat(Kd),
+            maxHvacOutput: 150,
           }),
         { headers: headers, method: "OPTIONS" }
       ).then((response) => {
@@ -30,18 +36,27 @@ export default function Graph() {
       console.log(data);
     }
     getData();
-  }, []);
-
-  return (
-    <Plot
-      data={[
-        {
-          x: [...data.times],
-          y: [...data.temps],
-          type: "scatter",
-          mode: "lines",
-        },
-      ]}
-    ></Plot>
-  );
+  }, [startingTemperature, desiredTemperature, Kp, Ki, Kd]);
+  if (data.times)
+    return (
+      <>
+        <Plot
+          data={[
+            {
+              x: [...data.times],
+              y: [...data.temps],
+              type: "scatter",
+              mode: "lines",
+            },
+          ]}
+        ></Plot>
+        <Fields
+          setStartingTemperature={setStartingTemperature}
+          setDesiredTemperature={setDesiredTemperature}
+          setKp={setKp}
+          setKi={setKi}
+          setKd={setKd}
+        ></Fields>
+      </>
+    );
 }
