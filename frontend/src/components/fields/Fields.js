6a035b12c7161a508b8ed63bccaf6c1ab0a46deb
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2022-12-10 16:01:36 +0100

    More selectors and code refactor

--- frontend/src/components/fields/Fields.js
+++ frontend/src/components/fields/Fields.js
@@ -1,85 +1,149 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Slider from "@mui/material/Slider";
 import styles from "./Fields.css";
+import {Box, Grid, Input, Select, Typography} from "@mui/material";
+
+function Selector(props) {
+    const {value, handleValueChange, title, name, minValue, maxValue, valueStep} = props;
+
+    return (
+        <Box sx={{width: 250}}>
+            <Typography id="input-slider" gutterBottom>
+                { title }
+            </Typography>
+            <Grid container spacing={2} alignItems="center">
+                <Grid item xs={9}>
+                    <Slider
+                        name={name}
+                        value={value}
+                        onChange={handleValueChange}
+                        aria-labelledby="input-slider"
+                        min={minValue}
+                        max={maxValue}
+                        step={valueStep}
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                    <Input
+                        value={value}
+                        name={name}
+                        size="small"
+                        onChange={handleValueChange}
+                        inputProps={{
+                            step: props.step,
+                            type: 'number',
+                            'aria-labelledby': 'input-slider',
+                            max: maxValue,
+                            min: minValue
+                        }}
+                    />
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
 
 export default function Fields(props) {
-  let marks = [];
-  for (let i = 0; i < 150; i++) {
-    marks.push({
-      value: i,
-      label: `${i}`,
-    });
-  }
+    const {parameters: {simulationTime, maxHVACOutput, startingTemperature, desiredTemperature, Kp, Ki, Kd}, handleChangeParameters} = props;
 
-  const handleValueChange = (event, newValue) => {
-    switch (event.target.name) {
-      case "startTemp":
-        props.setStartingTemperature(newValue);
-        break;
-      case "targetTemp":
-        props.setDesiredTemperature(newValue);
-        break;
-      case "Kp":
-        props.setKp(newValue);
-        break;
-      case "Kd":
-        props.setKd(newValue);
-        break;
-      case "Ki":
-        props.setKi(newValue);
-        break;
-      default:
-        console.log("error", event);
-    }
-  };
+    const marks = [...Array(150)].map((_, i) =>
+        {return {value: i, label: i.toString()}}
+    );
 
-  return (
-    <div className="slidersWrapper">
-      Temperatura początkowa
-      <Slider
-        name="startTemp"
-        aria-label="Custom marks"
-        defaultValue={0}
-        step={1}
-        valueLabelDisplay="auto"
-        onChange={handleValueChange}
-      />
-      Temperatura docelowa
-      <Slider
-        name="targetTemp"
-        aria-label="Custom marks"
-        defaultValue={0}
-        step={1}
-        valueLabelDisplay="auto"
-        onChange={handleValueChange}
-      />
-      Ki
-      <Slider
-        name="Ki"
-        aria-label="Custom marks"
-        defaultValue={0}
-        step={0.1}
-        valueLabelDisplay="auto"
-        onChange={handleValueChange}
-      />
-      Kp
-      <Slider
-        name="Kp"
-        aria-label="Custom marks"
-        defaultValue={0}
-        step={0.1}
-        valueLabelDisplay="auto"
-        onChange={handleValueChange}
-      />
-      Kd
-      <Slider
-        name="Kd"
-        aria-label="Custom marks"
-        defaultValue={0}
-        step={0.1}
-        valueLabelDisplay="auto"
-        onChange={handleValueChange}
-      />
-    </div>
-  );
+    const handleValueChange = e => {
+        const {target: {name, value}} = e;
+        switch (name) {
+            case "hvac":
+                handleChangeParameters({...props.parameters, maxHVACOutput: value});
+                break;
+            case "simulationTime":
+                handleChangeParameters({...props.parameters, simulationTime: value});
+                break;
+            case "startTemp":
+                handleChangeParameters({...props.parameters, startingTemperature: value});
+                break;
+            case "targetTemp":
+                handleChangeParameters({...props.parameters, desiredTemperature: value});
+                break;
+            case "Kp":
+                handleChangeParameters({...props.parameters, Kp: value});
+                break;
+            case "Ki":
+                handleChangeParameters({...props.parameters, Ki: value});
+                break;
+            case "Kd":
+                handleChangeParameters({...props.parameters, Kd: value});
+                break;
+            default:
+                console.log("error", e);
+        }
+    };
+
+    return (
+        <div className="slidersWrapper">
+            <Selector
+                title="Czas symulacji"
+                value={simulationTime}
+                handleValueChange={handleValueChange}
+                name="simulationTime"
+                minValue={0}
+                maxValue={180}
+                valueStep={1}
+            />
+            <Selector
+                title="Maksymalna moc grzewcza"
+                value={maxHVACOutput}
+                handleValueChange={handleValueChange}
+                name="hvac"
+                minValue={500}
+                maxValue={10_000}
+                valueStep={250}
+            />
+            <Selector
+                title="Temperatura początkowa"
+                value={startingTemperature}
+                handleValueChange={handleValueChange}
+                name="startTemp"
+                minValue={0}
+                maxValue={38}
+                valueStep={1}
+            />
+            <Selector
+                title="Temperatura docelowa"
+                value={desiredTemperature}
+                handleValueChange={handleValueChange}
+                name="targetTemp"
+                minValue={0}
+                maxValue={38}
+                valueStep={1}
+            />
+            <Selector
+                title="Kp"
+                value={Kp}
+                handleValueChange={handleValueChange}
+                name="Kp"
+                minValue={0}
+                maxValue={100}
+                valueStep={0.1}
+            />
+            <Selector
+                title="Ki"
+                value={Ki}
+                handleValueChange={handleValueChange}
+                name="Ki"
+                minValue={0}
+                maxValue={10}
+                valueStep={0.1}
+            />
+            <Selector
+                title="Kd"
+                value={Kd}
+                handleValueChange={handleValueChange}
+                name="Kd"
+                minValue={0}
+                maxValue={10}
+                valueStep={0.01}
+            />
+        </div>
+    );
 }
--- frontend/src/components/graph/graph.js
+++ frontend/src/components/graph/graph.js
@@ -1,62 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState, useMemo} from "react";
 import Plot from "react-plotly.js";
 import Fields from "../fields/Fields.js";
+import debounce from "lodash.debounce"
 import styles from "./graph.css";
-export default function Graph() {
-  const [data, setData] = useState({ graphData: [] });
-  const [startingTemperature, setStartingTemperature] = useState(0);
-  const [desiredTemperature, setDesiredTemperature] = useState(0);
-  const [Kp, setKp] = useState(1.0);
-  const [Ki, setKi] = useState(1.5);
-  const [Kd, setKd] = useState(2.0);
 
-  useEffect(() => {
-    async function getData() {
-      const headers = {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST,PATCH,OPTIONS",
-      };
-      const data = await fetch(
-        "http://localhost:8000/controller?" +
-          new URLSearchParams({
-            simulationTime: 5000,
-            startingTemperature: startingTemperature,
-            desiredTemperature: desiredTemperature,
-            Kp: parseFloat(Kp),
-            Ki: parseFloat(Ki),
-            Kd: parseFloat(Kd),
-            maxHvacOutput: 150,
-          }),
-        { headers: headers, method: "OPTIONS" }
-      ).then((response) => {
-        return response.json();
-      });
-      setData(data);
-      console.log(data);
-    }
-    getData();
-  }, [startingTemperature, desiredTemperature, Kp, Ki, Kd]);
-  if (data.times)
-    return (
-      <>
-        <Plot
-          data={[
-            {
-              x: [...data.times],
-              y: [...data.temps],
-              type: "scatter",
-              mode: "lines",
-            },
-          ]}
-        ></Plot>
-        <Fields
-          setStartingTemperature={setStartingTemperature}
-          setDesiredTemperature={setDesiredTemperature}
-          setKp={setKp}
-          setKi={setKi}
-          setKd={setKd}
-        ></Fields>
-      </>
-    );
+async function getData(setData, parameters) {
+            const headers = {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,POST,PATCH,OPTIONS",
+            };
+            const {simulationTime, maxHVACOutput, startingTemperature, desiredTemperature, Kp, Ki, Kd} = parameters;
+            const data = await fetch(
+                "http://localhost:8000/controller?" +
+                new URLSearchParams({
+                    simulationTime: simulationTime,
+                    startingTemperature: startingTemperature,
+                    desiredTemperature: desiredTemperature,
+                    Kp: parseFloat(Kp),
+                    Ki: parseFloat(Ki),
+                    Kd: parseFloat(Kd),
+                    maxHvacOutput: maxHVACOutput,
+                }),
+                {headers: headers, method: "OPTIONS"}
+            ).then((response) => {
+                return response.json();
+            });
+            setData(data);
+            console.count("Number of times getData was called");
+        }
+
+export default function Graph() {
+    const [data, setData] = useState({graphData: []});
+    const [parameters, setParameters] = useState({
+        simulationTime: 0,
+        maxHVACOutput: 0,
+        startingTemperature: 0,
+        desiredTemperature: 0,
+        Kp: 0,
+        Ki: 0,
+        Kd: 0
+    });
+    const debounceQuery = useMemo(() => debounce(() => getData(setData, parameters), 50), [parameters])
+
+    useEffect(() => {
+        debounceQuery()
+    }, [debounceQuery]);
+    if (data.times)
+        return (
+            <>
+                <Plot
+                    data={[
+                        {
+                            x: [...data.times.map(t => t / 60)],
+                            y: [...data.temps],
+                            type: "scatter",
+                            mode: "lines",
+                        },
+                    ]}
+                ></Plot>
+                <Fields
+                    parameters={parameters}
+                    handleChangeParameters={setParameters}
+                ></Fields>
+            </>
+        );
 }
