10f9e28b9206560b6e2c540613bfcc70b822f88c
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2022-12-15 20:30:32 +0100

    Prevent race conditions in useEffect
    https://github.com/facebook/react/issues/14326
    https://beta.reactjs.org/learn/you-might-not-need-an-effect#fetching-data

--- frontend/src/components/graph/graph.js
+++ frontend/src/components/graph/graph.js
@@ -1,38 +1,7 @@
-import React, {useEffect, useState, useMemo} from "react";
+import React, {useEffect, useState} from "react";
 import Plot from "react-plotly.js";
 import Fields from "../fields/Fields.js";
-import debounce from "lodash.debounce"
-import styles from "./graph.css";
 
-async function getData(setData, parameters) {
-            const headers = {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET,POST,PATCH,OPTIONS",
-            };
-            const {simulationTime, maxHVACOutput, startingTemperature, desiredTemperature, Kp, Ki, Kd} = parameters;
-            try {
-             const data = await fetch(
-                "http://localhost:8000/controller?" +
-                new URLSearchParams({
-                    simulationTime: simulationTime,
-                    startingTemperature: startingTemperature,
-                    desiredTemperature: desiredTemperature,
-                    Kp: parseFloat(Kp),
-                    Ki: parseFloat(Ki),
-                    Kd: parseFloat(Kd),
-                    maxHvacOutput: maxHVACOutput,
-                }),
-                {headers: headers}
-            )
-                if (data.status === 200){
-                    setData(await data.json());
-                }
-                console.count("Number of times getData was called");
-            } catch(e) {
-                console.error(e)
-            }
-        }
 
 export default function Graph() {
     const [data, setData] = useState({graphData: []});
@@ -45,11 +14,49 @@ export default function Graph() {
         Ki: 0,
         Kd: 0.05
     });
-    const debounceQuery = useMemo(() => debounce(() => getData(setData, parameters), 50), [parameters])
 
     useEffect(() => {
-        debounceQuery()
-    }, [debounceQuery]);
+        let didCancel = false;
+
+        async function getData(setData, parameters) {
+            const headers = {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,POST,PATCH,OPTIONS",
+            };
+            const {simulationTime, maxHVACOutput, startingTemperature, desiredTemperature, Kp, Ki, Kd} = parameters;
+            try {
+                const data = await fetch(
+                    "http://localhost:8000/controller?" +
+                    new URLSearchParams({
+                        simulationTime: simulationTime,
+                        startingTemperature: startingTemperature,
+                        desiredTemperature: desiredTemperature,
+                        Kp: parseFloat(Kp),
+                        Ki: parseFloat(Ki),
+                        Kd: parseFloat(Kd),
+                        maxHvacOutput: maxHVACOutput,
+                    }),
+                    {headers: headers}
+                )
+                if (didCancel)
+                    console.count("Cancelled request")
+                if (data.status === 200 && didCancel === false) {
+                    setData(await data.json());
+                    console.count("Number of times getData was called");
+                }
+            } catch (e) {
+                console.error(e)
+            }
+        }
+
+        getData(setData, parameters)
+
+        return () => {
+            didCancel = true;
+        }
+    }, [setData, parameters]);
+
     if (data.times)
         return (
             <>
