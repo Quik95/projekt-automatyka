95bdfa10d46ef458d196f14a2733e9cc99c128ae
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2022-12-16 19:30:18 +0100

    Move HVAC output to it's graph

--- frontend/src/components/graph/graph.js
+++ frontend/src/components/graph/graph.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import Plot from "react-plotly.js";
 import Fields from "../fields/Fields.js";
+import {Grid} from "@mui/material";
 
 
 export default function Graph() {
@@ -59,36 +60,52 @@ export default function Graph() {
 
     if (data.times)
         return (
-            <>
-                <Plot
-                    data={[
-                        {
-                            x: [...data.times.map(t => t / 60)],
-                            y: data.temps,
-                            type: "scatter",
-                            mode: "lines",
-                            name: "Current temperature"
-                        },
-                        {
-                            x: data.times.map(t => t / 60),
-                            y: data.hvac,
-                            type: "scatter",
-                            mode: "lines",
-                            name: "HVAC output"
-                        },
-                        {
-                            x: data.times.map(t => t / 60),
-                            y: data.times.map(_ => parameters.desiredTemperature),
-                            type: "scatter",
-                            mode: "lines",
-                            name: "Desired temperature",
-                        }
-                    ]}
-                ></Plot>
-                <Fields
-                    parameters={parameters}
-                    handleChangeParameters={setParameters}
-                ></Fields>
-            </>
+            <Grid container paddingX={"10px"} spacing={6} justifyContent="center" alignItems="center">
+                <Grid item xs={10}>
+                    <Grid container spacing={6} justiftContent="center">
+                        <Grid item>
+                            <Plot
+                                layout={{title: "Wykres temperatury w zależności od czasu"}}
+                                data={[
+                                    {
+                                        x: [...data.times.map(t => t / 60)],
+                                        y: data.temps,
+                                        type: "scatter",
+                                        mode: "lines",
+                                        name: "Current temperature"
+                                    },
+                                    {
+                                        x: data.times.map(t => t / 60),
+                                        y: data.times.map(_ => parameters.desiredTemperature),
+                                        type: "scatter",
+                                        mode: "lines",
+                                        name: "Desired temperature",
+                                    }
+                                ]}
+                            ></Plot>
+                        </Grid>
+                        <Grid item>
+                            <Plot
+                                layout={{title: "Wykres mocy systemu klimatyzacji w zależności od czasu"}}
+                                data={[
+                                    {
+                                        x: data.times.map(t => t / 60),
+                                        y: data.hvac,
+                                        type: "scatter",
+                                        mode: "lines",
+                                        name: "HVAC output"
+                                    },
+                                ]}
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={2}>
+                    <Fields
+                        parameters={parameters}
+                        handleChangeParameters={setParameters}
+                    ></Fields>
+                </Grid>
+            </Grid>
         );
 }
